827c9ec11f581a1f44520e5dfb5d7a6edba0288e
wlandau-lilly <will.landau@lilly.com>
2019-07-03 17:56:22 -0400

    Attempt to get jLaTeX pdf

--- build.R
+++ build.R
@@ -5,9 +5,13 @@ if (file.exists(dir)){
 }
 dir_create(dir)
 dir_copy("images", dir)
-options(drake_make_menu = FALSE)
-drake::clean(destroy = TRUE)
+options(drake_make_menu = FALSE, drake_clean_menu = FALSE)
 bookdown::render_book(
   input = "index.Rmd",
   output_format = "bookdown::gitbook"
+)
+bookdown::render_book(
+  input = "index.Rmd",
+  output_format = "bookdown::pdf_book",
+  output_dir = "pdfbook"
 )
